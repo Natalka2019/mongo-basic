8a1c2a738969d7ffcb3e7836425a21da2da0a627
Nataliya Rushchak <nrushchak@gmail.com>
2022-07-06 10:15:29 +0300

    users task

--- app.js
+++ app.js
@@ -1,34 +1,70 @@
-'use strict'
+'use strict';
 
-const {mapUser, getRandomFirstName} = require('./util')
+const {mapUser, getRandomFirstName} = require('./util');
 
 // db connection and settings
-const connection = require('./config/connection')
-let userCollection
-run()
+const connection = require('./config/connection');
+let userCollection;
+run();
 
 async function run() {
-  await connection.connect()
-  await connection.get().dropCollection('users')
-  await connection.get().createCollection('users')
-  userCollection = connection.get().collection('users')
+  await connection.connect();
+  await connection.get().dropCollection('users');
+  await connection.get().createCollection('users');
+  userCollection = connection.get().collection('users');
 
-  await example1()
-  await example2()
-  await example3()
-  await example4()
-  await connection.close()
+  await example1();
+  await example2();
+  await example3();
+  await example4();
+  await connection.close();
 }
 
 // #### Users
 
 // - Create 2 users per department (a, b, c)
 async function example1() {
-
   try {
-
+    await userCollection.insertMany([
+      {
+        firstName: 'Andrew',
+        lastName: 'Rayan',
+        department: 'a',
+        createdAt: new Date()
+      },
+      {
+        firstName: 'Leanne',
+        lastName: 'Graham',
+        department: 'a',
+        createdAt: new Date()
+      },
+      {
+        firstName: 'Ervin',
+        lastName: 'Howell',
+        department: 'b',
+        createdAt: new Date()
+      },
+      {
+        firstName: 'Clementine',
+        lastName: 'Bauch',
+        department: 'b',
+        createdAt: new Date()
+      },
+      {
+        firstName: 'Patricia',
+        lastName: 'Lebsack',
+        department: 'c',
+        createdAt: new Date()
+      },
+      {
+        firstName: 'Chelsey',
+        lastName: 'Dietrich',
+        department: 'c',
+        createdAt: new Date()
+      }
+    ]);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
@@ -36,9 +72,9 @@ async function example1() {
 
 async function example2() {
   try {
-
+    await userCollection.deleteOne({department: 'a'});
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
@@ -46,17 +82,19 @@ async function example2() {
 
 async function example3() {
   try {
-
+    await userCollection.updateMany({department: 'b'}, {$set: {firstName: 'Anna'}});
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
 // - Find all users from department (c)
 async function example4() {
   try {
+    const selection = await userCollection.find({department: 'c'}).toArray();
 
+    console.log(selection);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
